2fece61b13e8581d5ed7db0d05d7c490785ae6b5
rehanzukhi <rehanuddinuni@gmail.com>
2022-10-24 11:03:38 +0530

    Changes

--- Attendance/attendance/main/models.py
+++ Attendance/attendance/main/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length = 100)
+    present = models.BooleanField()
+    roll= models.CharField(max_length=10)
+    
+    
